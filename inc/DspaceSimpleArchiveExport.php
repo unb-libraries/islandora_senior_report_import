8dd2d1faa0ecbeee2d073949b4e1a834fdf48f07
Jacob Sanford <jsanford@unb.ca>
2014-08-18 21:41:23 -0300

    Add initial object constructors

---
+++ inc/DspaceSimpleArchiveExport.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains DspaceSimpleArchiveExport.
+ */
+
+/**
+ * Represents a newspaper import session.
+ *
+ * The DspaceSimpleArchiveExport class describes a full DSpace
+ * export in the Simple Archive format.
+ */
+class DspaceSimpleArchiveExport {
+  /**
+   * Constructs an DspaceSimpleArchiveExport object.
+   *
+   * @param string $import_path
+   *   The absolute path to the DSpace simple archive format export
+   */
+  protected function __construct($import_path) {
+    $this->importPath = $import_path;
+  }
+}
---
+++ inc/IslandoraSeniorReport.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains IslandoraSeniorReport.
+ */
+
+/**
+ * Represents a newspaper import session.
+ *
+ * The IslandoraSeniorReport class describes a Senior Report
+ * and handles its ingestion into Fedora
+ */
+class IslandoraSeniorReport {
+  /**
+   * Constructs an IslandoraSeniorReport object.
+   */
+  protected function __construct() {
+    // Pass
+  }
+}
